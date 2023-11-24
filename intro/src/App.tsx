f0749bbb16c9d61e2de44030491e40eb84229cdc
Bryan España <bryanespana21550@gmail.com>
2023-11-24 12:22:28 -0600

    Configuración proyecto y archivo login

---
+++ intro/src/App.tsx
@@ -0,0 +1,15 @@
+import { Login } from "./components/Login";
+
+ 
+ const App = () => {
+  return (
+    <div className='mt-2'>
+      <h1>Introducción a typescript</h1>
+      <hr />
+      <Login />
+    </div>
+  )
+}
+
+
+export default App;
